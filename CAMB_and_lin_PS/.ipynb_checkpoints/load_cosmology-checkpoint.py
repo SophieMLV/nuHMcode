ca17206c13ca4eb0ee73fede5474f4003a75557c
SophieMLV <79702672+SophieMLV@users.noreply.github.com>
2022-03-23 10:09:08 +0100

    correct description

--- CAMB_and_lin_PS/.ipynb_checkpoints/load_cosmology-checkpoint.py
+++ CAMB_and_lin_PS/.ipynb_checkpoints/load_cosmology-checkpoint.py
@@ -27,8 +27,6 @@ def load_cosmology_input(params_path):
             cosmo_dic['omega_d_0'] = total_dark - cosmo_dic['omega_nu_0']
             cosmo_dic['omega_db_0'] = cosmo_dic['omega_d_0'] + cosmo_dic['omega_b_0']
             cosmo_dic['omega_m_0'] = cosmo_dic['omega_db_0'] + cosmo_dic['omega_nu_0']
-        elif line[0].strip() == 'm_ax':
-            cosmo_dic['m_ax'] = eval(line[1].strip())
         elif line[0].strip() == 'h':
             cosmo_dic['h'] = eval(line[1].strip())
             cosmo_dic['Omega_b_0'] = cosmo_dic['omega_b_0']/cosmo_dic['h']**2
@@ -62,8 +60,6 @@ def load_LCDM_cosmology_input(params_path):
     """
     load the coresponding LCDM cosmology as in load_cosmology_inputs
     from the same input file in params_path
-    by ignoring the axion fraction and hardcode omega_ax_0 = 1e-20.
-    This is needed because axionCAMB need omega_ax_0 > 0
     """
     cosmo_dic = {}
     file = open(params_path)
@@ -78,8 +74,6 @@ def load_LCDM_cosmology_input(params_path):
             cosmo_dic['omega_db_0'] = cosmo_dic['omega_d_0'] + cosmo_dic['omega_b_0']
             cosmo_dic['omega_nu_0'] = 0
             cosmo_dic['omega_m_0'] = cosmo_dic['omega_db_0']
-        elif line[0].strip() == 'm_ax':
-            cosmo_dic['m_ax'] = eval(line[1].strip())
         elif line[0].strip() == 'h':
             cosmo_dic['h'] = eval(line[1].strip())
             cosmo_dic['Omega_b_0'] = cosmo_dic['omega_b_0']/cosmo_dic['h']**2
